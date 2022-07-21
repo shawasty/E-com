07f68f894ee625c24ced2feea3711b07d1ab9a9c
shawasty <shawasty@gmail.com>
2022-07-20 23:53:00 -0400

    edited routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const router = require( "express").Router();
 
-
-
+//REGISTER
 
 module.exports = router
--- routes/product.js
+++ routes/product.js
@@ -2,5 +2,4 @@ const router = require( "express").Router();
 
 
 
-
 module.exports = router
