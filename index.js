efacc78f470e5ddabb1ac0e04c6cda52edc6772d
shawasty <shawasty@gmail.com>
2022-07-20 22:27:13 -0400

    created database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express =  require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+const mongoose = require("mongoose");
+
+mongoose.connect ("mongodb+srv://shawasty:<password>@cluster0.twhjc.mongodb.net/?retryWrites=true&w=majority")
+
 const app = express();
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on port${PORT} `)
-});
+app.listen(PORT,()=>{
+    console.log(`Express server listening on port: ${PORT}`);
+})
